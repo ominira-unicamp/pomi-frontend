58a74e46d412999b8596c028080cb26821e30f62
GoliasVictor <dev.jvsb@gmail.com>
2026-08-11 06:59:51 -0300

    fix: remove carregamento din??mico de web vitals

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,6 @@ import { ThemeProvider } from './components/ThemeProvider.tsx'
 import { routeTree } from './routeTree.gen'
 
 import './styles.css'
-import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
 
@@ -49,8 +48,3 @@ if (rootElement && !rootElement.innerHTML) {
     </StrictMode>,
   )
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log)
